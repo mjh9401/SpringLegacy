16107b732898cebb8b133aa98f2758202d9f1104
mjh9401@naver.com <mjh9401@naver.com>
2023-10-15 17:22:08 +0900

    디스패처 서블릿 설정완료

---
+++ src/main/java/webprj/IndexController.java
@@ -0,0 +1,21 @@
+package webprj;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class IndexController implements Controller{
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("data","Hello Spring MVC~");
+		//mv.setViewName("/WEB-INF/view/index.jsp");
+		mv.setViewName("index");
+		
+		return mv;
+	}
+	
+}
